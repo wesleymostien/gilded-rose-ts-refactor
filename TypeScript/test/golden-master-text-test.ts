ec6fb8830faa12f784803d674b5369b484bd23d1
Wesley Mostien <wesley.mostien@doccle.be>
2025-04-11 18:00:05 +0200

    Made tests work with texttest + python

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -1,6 +1,6 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import { Item, GildedRose } from "../app/gilded-rose";
 
-console.log("OMGHAI!")
+console.log("OMGHAI!");
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20), //
@@ -12,22 +12,21 @@ const items = [
   new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
   new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
   // this conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6)];
-
+  new Item("Conjured Mana Cake", 3, 6),
+];
 
 const gildedRose = new GildedRose(items);
 
-let days: number = 2;
+let days: number = 30;
 if (process.argv.length > 2) {
-    days = +process.argv[2];
-  }
+  days = +process.argv[2];
+}
 
 for (let i = 0; i < days + 1; i++) {
   console.log("-------- day " + i + " --------");
   console.log("name, sellIn, quality");
-  items.forEach(element => {
-    console.log(element.name + ', ' + element.sellIn + ', ' + element.quality);
-
+  items.forEach((element) => {
+    console.log(element.name + ", " + element.sellIn + ", " + element.quality);
   });
   console.log();
   gildedRose.updateQuality();
